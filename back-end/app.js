59327a0255a981b321723c35ae78fa469ca34c9b
CairoVieira <vieira.cairo@hotmail.com>
2020-10-02 23:19:05 +0000

    Model, Controller e CRUD de Planta

--- back-end/app.js
+++ back-end/app.js
@@ -23,4 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const planta = require('./routes/planta')
+app.use('/planta', planta)
+
 module.exports = app;
---
+++ back-end/controllers/planta.js
@@ -0,0 +1,64 @@
+const Planta = require('../models/Planta')
+
+const controller = {}
+
+controller.novo = async (req, res) => {
+    try {
+        await Planta.create(req.body)
+        res.status(201).end()
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).send(erro)
+    }
+}
+
+controller.listar = async (req, res) => {
+    try {
+        let dados = await Planta.find()
+        res.send(dados)
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).send(erro)
+    }
+}
+
+controller.obterUm = async (req, res) => {
+    const id = req.params.id
+    let obj = await Planta.findById(id)
+
+    if (obj) res.send(obj)
+    else res.status(404).end()
+
+}
+
+controller.atualizar = async (req, res) => {
+    try {
+        const id = req.body._id
+        let obj = await Planta.findByIdAndUpdate(id, req.body)
+
+        if (obj) res.status(204).end()
+        else res.status(404).end()
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).end()
+    }
+}
+
+controller.excluir = async (req, res) => {
+    try {
+        const id = req.body._id
+        let obj = await Planta.findByIdAndDelete(id)
+
+        if(obj) res.status(204).end()
+        else res.status(404).end()
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).send(erro)
+    }
+}
+
+module.exports = controller
